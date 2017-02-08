dc8152ef3fa0859ac92f3289e7fd3c6ed1efc599
Kaplaugher <kaplaugher94@gmail.com>
2017-02-08 13:16:13 -0500

    updates

--- config/config.js
+++ config/config.js
@@ -2,6 +2,6 @@ module.exports = {
   host: "127.0.0.1",
   username: "x",
   password: "x",
-  database: "todo"
+  database: "endangeredSpecies"
 
 };
--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,52 @@ var connection = mysql.createConnection({
   database : config.database
 });
 
+
+// After this line runs, we are connected to MySQL!
+connection.connect();
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/page2', function(req, res, next) {
+  res.render('page2', { title: 'Express' });
+});
+
+router.get('/page3', function(req, res, next) {
+  res.render('page3', { title: 'Express' });
+});
+
+router.get('/page4', function(req, res, next) {
+  res.render('page4', { title: 'Express' });
+});
+
+router.get('/page5', function(req, res, next) {
+  res.render('page5', { title: 'Express' });
+});
+
+router.get('/page6', function(req, res, next) {
+  res.render('page6', { title: 'Express' });
+});
+
+router.get('/page7', function(req, res, next) {
+  res.render('page7', { title: 'Express' });
+});
+
+router.get('/page8', function(req, res, next) {
+  res.render('page8', { title: 'Express' });
+});
+
+router.get('/page9', function(req, res, next) {
+  res.render('page9', { title: 'Express' });
+});
+
+router.get('/page10', function(req, res, next) {
+  res.render('page10', { title: 'Express' });
+});
+
+
 
 module.exports = router;
